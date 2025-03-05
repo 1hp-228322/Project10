3a901a1c3dc2d8f522dc9a68b0b2baf85c69207f
1hp <keshasuper396@gmail.com>
2025-03-05 23:21:36 +0300

    18 25 26 27 28 29 30

--- Wpfchik/Pages/Page29.xaml.cs
+++ Wpfchik/Pages/Page29.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using Project10.Classes;
 
 namespace Wpfchik.Pages
@@ -24,6 +13,23 @@ namespace Wpfchik.Pages
         public Page29()
         {
             InitializeComponent();
+        }
+
+        private void BtnTask11_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(TbN1.Text) && string.IsNullOrEmpty(TbM1.Text))
+            {
+                MessageBox.Show("Нет данных!", "Системное сообщение", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else
+            {
+                //double G = Math.Exp(2 * Convert.ToDouble(TbY.Text)) + Math.Sin(Convert.ToDouble(Tbf.Text)) / Math.Log10(3.8 * Convert.ToDouble(TbY.Text) + Convert.ToDouble(Tbf.Text));
+                Class29 class7 = new Class29(Convert.ToInt32(TbN1.Text), Convert.ToInt32(TbM1.Text));
+
+                MessageBox.Show($"Ответ = {class7.CalculateProductOfSquares()}", "Системное сообщение", MessageBoxButton.OK, MessageBoxImage.Information);
+                TbN1.Text = string.Empty;
+                TbM1.Text = string.Empty;
+            }
         }
     }
 }
