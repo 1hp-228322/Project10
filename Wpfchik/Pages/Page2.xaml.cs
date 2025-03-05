51b6fb4fbb32244289b6ca643819e483280039c4
1hp <keshasuper396@gmail.com>
2025-03-05 21:25:54 +0300

    ssd

--- Wpfchik/Pages/Page2.xaml.cs
+++ Wpfchik/Pages/Page2.xaml.cs
@@ -25,5 +25,20 @@ namespace Wpfchik.Pages
         {
             InitializeComponent();
         }
+        private void BtnTask11_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(TbN1.Text))
+            {
+                MessageBox.Show("Нет данных!", "Системное сообщение", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else
+            {
+                //double G = Math.Exp(2 * Convert.ToDouble(TbY.Text)) + Math.Sin(Convert.ToDouble(Tbf.Text)) / Math.Log10(3.8 * Convert.ToDouble(TbY.Text) + Convert.ToDouble(Tbf.Text));
+                Class2 class2 = new Class2(Convert.ToInt32(TbN1.Text));
+
+                MessageBox.Show($"Ответ = {class2.G1()}", "Системное сообщение", MessageBoxButton.OK, MessageBoxImage.Information);
+                TbN1.Text = string.Empty;
+            }
+        }
     }
 }
